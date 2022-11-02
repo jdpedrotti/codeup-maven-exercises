420c93568e21041f6f0099602c0930b6cbdd5151
John Pedrotti <jdpedrotti@gmail.com>
2022-11-02 11:51:19 -0500

    complete maven ex with simplified code

--- src/main/java/MavenExercise.java
+++ src/main/java/MavenExercise.java
@@ -7,27 +7,23 @@ import static org.apache.commons.lang3.StringUtils.isNumeric;
 public class MavenExercise {
 
     public static void main(String[] args) {
-        String prompt = "Type something: ";
-        System.out.println(prompt);
-
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println( "Type something: ");
+
         String userInput = scanner.nextLine();
-        String promptEntry = "You entered: " + userInput;
-        System.out.println(promptEntry);
+        System.out.println("You entered: " + userInput);
 
         boolean notNum = StringUtils.isNumeric(userInput);
             if (notNum){
-                System.out.println(userInput + " contains a number");
+                System.out.println(userInput + " is a number");
             } else {
                 System.out.println(userInput + " is not a number");
             }
 
-        String flipCase = StringUtils.swapCase(userInput);
-        System.out.println("Flipped case: " + flipCase);
+        System.out.println("Flipped case: " + StringUtils.swapCase(userInput));
 
-        String reverseString = StringUtils.reverse(userInput);
-        System.out.println("Reversed: " + reverseString);
+        System.out.println("Reversed: " + StringUtils.reverse(userInput));
 
     }
 
